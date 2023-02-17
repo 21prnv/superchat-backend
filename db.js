43d1d61367df3fc65f5bb2f0655b8fa42c52036c
21prnv <pranavpatil6251@gmail.com>
2023-02-17 14:25:36 +0530

    df

--- db.js
+++ db.js
@@ -1,1 +1,12 @@
 const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.mongo_URL)
+  .then((req, res) => {
+    console.log("connect to database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to database" + err);
+  });
